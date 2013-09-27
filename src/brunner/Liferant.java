6f9c007a5062687fa60953c189e5dfec3187c134
Helmuth Brunner <Helmuth.B@gmx.at>
2013-09-27 14:26:21 +0200

    Alles Threedea

---
+++ src/brunner/Liferant.java
@@ -0,0 +1,10 @@
+package brunner;
+
+public class Liferant implements Runnable {
+
+	@Override
+	public void run() {
+		
+	}
+
+}
---
+++ src/brunner/Montuer.java
@@ -0,0 +1,12 @@
+package brunner;
+
+public class Montuer implements Runnable {
+
+	@Override
+	public void run() {
+		/**
+		 * Im Lager nachschauen und Teile holen
+		 */
+	}
+
+}
