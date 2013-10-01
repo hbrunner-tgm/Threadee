69213681f35964d29c2e9daf275056d35a7cb60e
awillinger <awi95@gmx.at>
2013-10-01 15:21:33 +0200

    Stoppable Interface hinzugefügt

---
+++ src/tgm/Stoppable.java
@@ -0,0 +1,14 @@
+package tgm;
+
+/**
+ * Stoppable Klasse, wird hauptstächlich von Watchdog(s) benutzt
+ *
+ * @author Andreas Willinger
+ * @version 0.1
+ * @since 01.10.13 15:19
+ */
+public interface Stoppable
+    extends Runnable
+{
+    public void stop();
+}
