99f2cba7dfed86934f33f1982ee02ddeff7d1d96
Helmuth Brunner <Helmuth.B@gmx.at>
2013-10-02 19:28:37 +0200

    Aenderungen an Monteur und Lieferant

--- src/tgm/Lieferant.java
+++ src/tgm/Lieferant.java
@@ -2,40 +2,46 @@ package tgm;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Lieferant implements Stoppable
 {
-    private final static Logger logger = Logger.getLogger("Lieferant");
-    private Lagermitarbeiter lagermitarbeiter;
+	private final static Logger logger = Logger.getLogger("Lieferant");
+	private Lagermitarbeiter lagermitarbeiter;
+	private boolean running;
+	private Random r;
 
-    public Lieferant(String pfadLog, Lagermitarbeiter lagermitarbeiter)
-    {
-        this.lagermitarbeiter = lagermitarbeiter;
+	public Lieferant(String pfadLog, Lagermitarbeiter lagermitarbeiter)
+	{
+		this.lagermitarbeiter = lagermitarbeiter;
+		this.running=true;
+		this.r= new Random();
 
-        pfadLog += "lieferant.log";
-        try
-        {
-            File f = new File(pfadLog);
-            if(!f.exists()) f.createNewFile();
+		pfadLog += "lieferant.log";
+		try
+		{
+			File f = new File(pfadLog);
+			if(!f.exists()) f.createNewFile();
 
-            FileHandler fh = new FileHandler(pfadLog);
-            SimpleFormatter formatter = new SimpleFormatter();
-            fh.setFormatter(formatter);
-            logger.addHandler(fh);
+			FileHandler fh = new FileHandler(pfadLog);
+			SimpleFormatter formatter = new SimpleFormatter();
+			fh.setFormatter(formatter);
+			logger.addHandler(fh);
 
-        }
-        catch (IOException e)
-        {
-            System.out.println("File logging disabled");
-        }
-    }
+		}
+		catch (IOException e)
+		{
+			System.out.println("File logging disabled");
+		}
+	}
 
 	@Override
 	public void run() {
-        /*
+		/*
 
         BEISPIEL:
 
@@ -57,10 +63,66 @@ public class Lieferant implements Stoppable
         }
         System.out.println("*** KILLED (LIEFERANT)");
 
-         */
+		 */
+
+		while(true) {
+			
+			switch(r.nextInt(4)+1)
+			{
+			case 1:
+				this.lagermitarbeiter.addTeil(Lagermitarbeiter.ETeil.TEIL_ARM, this.pgnr());
+			case 2:
+				this.lagermitarbeiter.addTeil(Lagermitarbeiter.ETeil.TEIL_AUGE, this.pgnr());
+			case 3:
+				this.lagermitarbeiter.addTeil(Lagermitarbeiter.ETeil.TEIL_KETTENANTRIEB, this.pgnr());
+			case 4:
+				this.lagermitarbeiter.addTeil(Lagermitarbeiter.ETeil.TEIL_RUMPF, this.pgnr());
+			}
+			
+			try {
+				Thread.sleep(this.getNextDeliveryTime());
+			} catch (InterruptedException e) {
+				logger.log(Level.WARNING, "Thread wurde im Schlaf unterbrochen!");
+			}
+			
+		}
+
 	}
 
-    public void stop()
-    {
-    }
+	/**
+	 * Eine Methode die Random-Numbers genriert für das LagerFile
+	 * @return als Text die Random-Numbers
+	 */
+	public String pgnr() {
+		r.setSeed(78160);
+		String str="";
+
+		int[] array= new int[20];
+
+		for(int i=0;i<array.length;i++) {
+			if(i!=array.length-1)
+				str=str+ r.nextInt(200)+",";
+			else
+				str=str+ r.nextInt(200);
+		}
+
+		return str;
+	}
+
+	/**
+	 * Liefert die Zeit die dann den Thread kurz pausiert.
+	 * @return die neue Zeit
+	 */
+	private long getNextDeliveryTime() {
+		long out;
+		do{
+			out= (long) r.nextInt(2000);
+		}while(out<100 && out % 10 >0);
+		return out;
+	}
+	
+	public void stop() {
+		this.running = false;
+		logger.log(Level.INFO, "Lieferant arbeit beendet!");
+	}
 }
--- src/tgm/Monteur.java
+++ src/tgm/Monteur.java
@@ -2,7 +2,9 @@ package tgm;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
@@ -12,11 +14,14 @@ public class Monteur implements Stoppable
     private int id;
 	private Lagermitarbeiter lagermitarbeiter;
     private Sekretariat sekretariat;
+    private boolean running;
+    private int[] armsorted, augesorted, kettensorted, rumpfsorted;
 
 	public Monteur(String pfadLog, int id, Lagermitarbeiter lagermitarbeiter, Sekretariat sekretariat) {
 		this.id = id;
         this.lagermitarbeiter = lagermitarbeiter;
         this.sekretariat = sekretariat;
+        this.running=true;
 
         pfadLog += "monteur.log";
         try
@@ -39,10 +44,54 @@ public class Monteur implements Stoppable
 	@Override
 	public void run()
     {
+		
+		String[] arrayarm, arrayauge, arrayketten, arrayrumpf;
+		
+		while(true)
+		{
+			
+			arrayarm= this.lagermitarbeiter.getTeil(Lagermitarbeiter.ETeil.TEIL_ARM).split(",");
+			arrayauge= this.lagermitarbeiter.getTeil(Lagermitarbeiter.ETeil.TEIL_AUGE).split(",");
+			arrayketten= this.lagermitarbeiter.getTeil(Lagermitarbeiter.ETeil.TEIL_KETTENANTRIEB).split(",");
+			arrayrumpf= this.lagermitarbeiter.getTeil(Lagermitarbeiter.ETeil.TEIL_RUMPF).split(",");
+
+			
+			armsorted= new int[arrayarm.length-1];
+			augesorted= new int[arrayauge.length-1];
+			kettensorted= new int[arrayketten.length-1];
+			rumpfsorted= new int[arrayrumpf.length-1];
+			
+			for(int i=1;i<armsorted.length;i++) {
+				armsorted[i]= Integer.parseInt(arrayarm[i]);
+			}
+			for(int i=1;i<augesorted.length;i++) {
+				augesorted[i]= Integer.parseInt(arrayauge[i]);
+			}
+			for(int i=1;i<kettensorted.length;i++) {
+				kettensorted[i]= Integer.parseInt(arrayketten[i]);
+			}
+			for(int i=1;i<rumpfsorted.length;i++) {
+				rumpfsorted[i]= Integer.parseInt(arrayrumpf[i]);
+			}
+			
+			//Sortieren
+			
+			Arrays.sort(armsorted);
+			Arrays.sort(augesorted);
+			Arrays.sort(kettensorted);
+			Arrays.sort(rumpfsorted);
+			
+		}
+	}
+	
+	public void zusammenbauen() {
+		//Zusammen baue den Threedeas
 	}
 
     @Override
     public void stop()
     {
+    	this.running=false;
+    	logger.log(Level.INFO, "Monteur arbeit beendet!");
     }
 }
